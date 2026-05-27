98146053b245b160576620b2b6cae6ba8392bf60
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-05-27 12:58:09 +0530

    Configure backend and frontend for vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import router from './routes/userRoutes.js';
-import cors from 'cors';
-dotenv.config();
-const app = express();
-app.use(cors());
-app.use(express.json());  
-app.use('/api/users', router); 
-const PORT = process.env.PORT || 5000;  
-const mongoURI = process.env.MONGO_URI;
- 
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import router from './routes/userRoutes.js'
+import cors from 'cors'
+
+dotenv.config()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/users', router)
+
+const mongoURI = process.env.MONGO_URI
+
 mongoose.connect(mongoURI)
   .then(() => {
-    console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port http://localhost:${PORT}`);
-    });
+    console.log('Connected to MongoDB')
   })
-  .catch(err => console.log(err));
+  .catch((err) => {
+    console.log(err)
+  })
+
+export default app
