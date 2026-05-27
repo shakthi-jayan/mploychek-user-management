15e3ce082f95e6badc2d216176ab7127b6aaaddb
Shakthi Jayan J <shakthijayan19@gmail.com>
2026-05-27 14:38:06 +0530

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,20 @@
 import express from 'express';
 const router = express.Router();
-import { getAllUsers, addUser, loginUser } from '../controllers/userController.js';
-import { getDashboardData } from '../controllers/dashboardController.js';
+
+import {
+  getAllUsers,
+  addUser,
+  loginUser
+} from '../controllers/userController.js';
+
+import {
+  getDashboardData
+} from '../controllers/dashboardController.js';
 
 router.get('/', getAllUsers);
-router.post('/add', addUser); 
+router.post('/add', addUser);
 router.post('/login', loginUser);
 
-// Dashboard route
 router.get('/dashboard', getDashboardData);
 
 export default router;
